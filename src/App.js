b54c05027b4ffae6fae885638cb003c7971572c6
saicharitha6 <saicharitha1932@gmail.com>
2024-05-07 13:01:02 +0530

    init with api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await axios.post("https://api.weekday.technology/adhoc/getSampleJdJSON", {
+          limit: 10,
+          offset: 0
+        });
+        if (response.status === 200) {
+          setJobs(response.data.jdList); // Assuming response.data contains the list of jobs
+        } else {
+          throw new Error('Failed to fetch jobs');
+        }
+      } catch (error) {
+        console.error('Error fetching jobs:', error);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+  console.log('Jobs State:', jobs); 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>List of Jobs</h1>
+      <div className="job-cards-container">
+        {jobs.map(job => (
+          <div className="job-card" key={job.id}>
+            <h2>{job.jobRole}</h2>
+            <p>{job.description}</p>
+            {/* Include more details from the job object */}
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
